1c59c3d4c3a5d5ac98782b986f6b614689594087
Juicycool <100525337+sean2337@users.noreply.github.com>
2022-10-10 22:39:01 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+	int input;
+	cin >> input;
+
+	//홀수라면 창영이가 이기고, 짝수면 상근이가 이기는 구조이다.
+	//이는 코드가 어렵다기 보다는 직접 경우의 수를 나열하는 것이 중요한 문제
+
+
+	if (input % 2 == 0) {
+		cout << "SK";
+	}
+	else {
+		cout << "CY";
+	}
+
+
+
+	return 0;
+}
