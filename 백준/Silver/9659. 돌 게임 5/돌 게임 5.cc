99ad5938a03eb39721495fde5a5f2964b9f13e72
Juicycool <100525337+sean2337@users.noreply.github.com>
2022-11-01 00:51:15 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main() {
+	long long inputNum;
+	
+	cin >> inputNum;
+
+	if (inputNum % 2 == 1) {
+		cout << "SK";
+	}
+	else {
+		cout<<"CY";
+	}
+
+
+
+	return 0;
+}
