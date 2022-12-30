0d8b218c4c013fce3987680f84e1607efe6d0df4
Juicycool <100525337+sean2337@users.noreply.github.com>
2022-12-30 16:14:20 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+  var answer = [0,0];
+  num_list.forEach(element => {
+    if(element%2==0){
+      answer[0]++;
+    }
+    else{
+      answer[1]++;
+    }
+  });
+  return answer;
+}
