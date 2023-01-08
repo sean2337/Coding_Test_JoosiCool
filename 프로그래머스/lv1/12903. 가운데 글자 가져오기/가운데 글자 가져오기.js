ddaf151a533ddea758c7ec5a2d6761ca46666691
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-01-08 15:39:18 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  var answer = "";
+
+  if (s.length % 2 === 1) {
+    answer = s[(s.length - 1) / 2];
+  } else {
+    answer += s[s.length / 2 - 1] + s[s.length / 2];
+  }
+
+  return answer;
+}
