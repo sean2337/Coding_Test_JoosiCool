e5771eb8af55a034a6579e8425e17b01e551982e
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-01-09 16:34:08 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.58 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 1.03 ms
+메모리: 33.9 MB, 시간: 0.58 ms
 
 ### 구분
 
--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,35 +1,25 @@
 function solution(numbers) {
-    var answer = [];
-    var result = [];
-    //모든 요소 더한 값 구하기
-    for(var one = 0;one<numbers.length-1;one++){
-        for(var two = one+1;two<numbers.length;two++){
-            answer.push(numbers[one]+numbers[two]);
-        }
+  var answer = [];
+  //모든 요소 더한 값 구하기
+  for (var one = 0; one < numbers.length - 1; one++) {
+    for (var two = one + 1; two < numbers.length; two++) {
+      answer.push(numbers[one] + numbers[two]);
     }
-    
-    //오름차순으로 정렬
-    answer = answer.sort((a,b)=>{
-        if(a>b){
-            return 1;
-        }
-        else{
-            return -1;
-        }
-    });
-    
-    
-    //중복된거 빼기
-    result.push(answer[0]);
-    var before = answer[0];
-    for(var i =1;i<answer.length;i++){
-        if(before !== answer[i]){
-            result.push(answer[i]);
-        }
-        before = answer[i];
+  }
+
+  //오름차순으로 정렬
+  answer = answer.sort((a, b) => {
+    if (a > b) {
+      return 1;
+    } else {
+      return -1;
     }
-    
-    
-    
-    return result;
+  });
+
+  //중복된거 빼기
+  answer = [...new Set(answer)];
+
+  return answer;
 }
+
+console.log(solution([2, 1, 3, 4, 1]));
