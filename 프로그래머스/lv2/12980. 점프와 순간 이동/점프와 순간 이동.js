b971bd2cf0645a9fc06b9d969a6e6c84b68a8301
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-02-09 10:03:25 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,12 @@
+function solution(n)
+{
+    var ans = 1;
+    while(n!==1){
+        if(n%2===0) n = n/2;
+        else{
+            n=n-1;
+            ans++;
+        }
+    }
+    return ans;
+}
