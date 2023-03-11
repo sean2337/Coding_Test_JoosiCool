328f564502cf8428d5b9b7b6e07aacff95961932
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-03-11 15:31:42 +0900

    [level 2] Title: 방문 길이, Time: 0.42 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,41 @@
+function solution(dirs) {
+    //중복을 제거하기 위해, Set사용
+    var set = new Set();
+    var location = [0,0];
+    dirs = dirs.split("");
+    var plus;
+    dirs.forEach((element)=>{
+        // 00 -> 11 = 0011푸쉬
+        if(element==='U'&&location[1]<5){
+            plus = ""+location[0] + location[1] + location[0] + (location[1]+1);
+            set.add(plus);
+            plus = "" + location[0] + (location[1]+1)+location[0] + location[1];
+            set.add(plus);
+            location[1]++;
+        }
+        else if(element ==='D'&&location[1]>-5){
+            plus = ""+location[0] + location[1] + location[0] + (location[1]-1);
+            set.add(plus);
+            plus = ""+ location[0] + (location[1]-1)+location[0] + location[1];
+            set.add(plus);
+            location[1]--;
+        }
+        else if(element ==='R'&&location[0]<5){
+            plus = ""+location[0] + location[1] + (location[0]+1) + location[1];
+            set.add(plus);
+            plus = "" + (location[0]+1) + location[1]+location[0] + location[1];
+            set.add(plus);
+            location[0]++;
+        }
+        else if(element ==='L'&&location[0]>-5){
+            plus = ""+location[0] + location[1] + (location[0]-1) + location[1];
+            set.add(plus);
+            plus = ""+ (location[0]-1) + location[1]+location[0] + location[1];
+            set.add(plus);
+            location[0]--;
+        }
+    })
+    return Math.ceil(set.size/2);
+}
+
+console.log(solution("ULURRDLLU"));
