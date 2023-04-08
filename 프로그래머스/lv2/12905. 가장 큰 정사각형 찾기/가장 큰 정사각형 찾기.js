57c1b7539a8fdd68b8c09fd5cb458574207cc9d6
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-04-08 23:34:10 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 34.47 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -1,25 +1,33 @@
-
-
-
 function solution(board)
 {
+    
+
+    
     var row = board.length;
     var col = board[0].length;
+    if(row===1 && col === 1){
+        if(board[0][0]===0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
     var answer = 0;   
-    var map = new Array(row+1);
-    for(var i = 0;i<=row;i++){
-        map[i] = new Array(col+1).fill(0);
+    var map = new Array(row);
+    for(var i = 0;i<row;i++){
+        map[i] = new Array(col).fill(0);
     }
 
     
     for(var i = 0;i<row;i++){
         for(var k = 0 ;k<col;k++){
-            map[i+1][k+1] = board[i][k]
+            map[i][k] = board[i][k]
         }
     }
 
-    for(var r = 1;r<=row;r++){
-        for(var c = 1;c<=col;c++){
+    for(var r = 1;r<row;r++){
+        for(var c = 1;c<col;c++){
             if(map[r][c]!==0){
                 map[r][c] = Math.min(map[r-1][c],map[r][c-1],map[r-1][c-1])+1;
                 answer = Math.max(answer,map[r][c]);
