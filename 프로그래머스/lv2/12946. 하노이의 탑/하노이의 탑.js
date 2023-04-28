6d2ecd391643ae9dc2159670422d4a9f43c644a6
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-04-28 14:46:22 +0900

    [level 2] Title: 하노이의 탑, Time: 8.38 ms, Memory: 49 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,24 +1,15 @@
-
-
 function solution(n) {
-    
     var answer = [];
     
-    function Hanoi(num, from, other, to){
-    if(num === 0) {
-        return;
-    }else{
-        // 받아온 원반 갯수보다 하나 적은 원반들을 목적지가 아닌 곳으로 재귀적으로 이동
-        Hanoi(num - 1 , from, to, other);
-        // 맨 아래 원반을 목적지로 이동시킴
-        answer.push([from, to]);
-        //다른 곳으로 옮겼던 원반들을 그 위에 얹음
-        Hanoi(num - 1, other, from, to);
+    function Hanoi(num,from,to,other){
+        if(num==0)return;
+        
+        Hanoi(num-1,from,other,to);
+        answer.push([from,to]);
+        Hanoi(num-1,other,to,from);
     }
-}
     
+    Hanoi(n,1,3,2);
     
-    Hanoi(n,1,2,3);
-
     return answer;
 }
