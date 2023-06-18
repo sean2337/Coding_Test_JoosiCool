a8356ad9a0a19409182a8b0db79f432dcdb5f8e4
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-06-18 17:09:41 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,41 +1,46 @@
-function solution(id_list, report, check) {
-  var answer = new Array(id_list.length).fill(0);
+function solution(id_list, report, k) {
+    var answer = [];
+    //신고당한 사람 기준에서 신고한 사람의 set을 가지고 있음
+    var map = new Map();
+    // 신고한 사람 기준 => 신고당한 사람의 set을 가지고 있음
+    var toMap = new Map();
+    
+    //각각에 대한 해시테이블 만들기 key= id, value= 신고한 사람의 이름을 담을 셋
+    id_list.forEach((element)=>{
+        map.set(element, new Set());
+    })
 
-  var declationArr = [];
-  var cancelArr = [];
-  //신고 당한것을 카운트할 배열 생성
-  for (var i = 0; i < id_list.length; i++) {
-    var plusArr = [];
-    for (var k = 0; k < id_list.length; k++) {
-      plusArr.push(0);
-    }
-    declationArr.push(plusArr);
-    cancelArr.push(0);
-  }
-
-  //[ [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ]
-  //신고 카운트
-  for (var i = 0; i < report.length; i++) {
-    declationArr[id_list.indexOf(report[i].split(" ")[0])][
-      id_list.indexOf(report[i].split(" ")[1])
-    ]++;
-    if (
-      declationArr[id_list.indexOf(report[i].split(" ")[0])][
-        id_list.indexOf(report[i].split(" ")[1])
-      ] === 1
-    ) {
-      cancelArr[id_list.indexOf(report[i].split(" ")[1])]++;
-    }
-  }
-
-  //메일 보낼 횟수 선정
-  for (var i = 0; i < declationArr.length; i++) {
-    for (var k = 0; k < declationArr[i].length; k++) {
-      if (declationArr[i][k] !== 0 && cancelArr[k] >= check) {
-        answer[i]++;
-      }
-    }
-  }
-
-  return answer;
+    //id = 신고한 사람의 id, value=신고 당한 사람의 모음
+    id_list.forEach((element)=>{
+        toMap.set(element, new Set());
+    })
+    
+    //신고자에 따른 값 추가
+    report.forEach((element)=>{
+        element = element.split(" ");
+        //신고한 사람
+        var toPlayer = element[0];
+        //신고 당한 사람
+        var fromPlayer = element[1];
+        
+        map.set(fromPlayer, map.get(fromPlayer).add(toPlayer));
+        toMap.set(toPlayer, toMap.get(toPlayer).add(fromPlayer));
+        
+    })
+    
+    
+    // set에 담긴 크기가 k이상인 아이디 걸러내기
+    id_list.forEach((element)=>{
+        var count = 0;
+        //신고한 사람이 누굴 신고했는지 다음 set에서 신고당한 사람 한명씩 가져오기
+        for(var fromPlayer of toMap.get(element)){
+            if((map.get(fromPlayer)).size>=k){
+                count++;
+            }
+        }
+        answer.push(count);
+    })
+    
+    
+    return answer;
 }
