d0218df20f5514ad41ad763ab1a4b232f1b4c1bd
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-06-23 14:50:38 +0900

    [unrated] Title: 옹알이 (2), Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,40 +1,42 @@
 function solution(babbling) {
-  var answer = 0;
-  var checkArr = ["aya", "ye", "woo", "ma"];
-  //babbling 하나씩 체크
-  for (var i = 0; i < babbling.length; i++) {
-    //각 요소가 checkArr에 해당하는 것이 있으면 숫자로 변경.
-    for (var check = 0; check < checkArr.length; check++) {
-      babbling[i] = babbling[i].replaceAll(checkArr[check], check);
+    var answer = 0;
+    var canArr = ["aya", "ye", "woo", "ma"];
+
+    
+    
+    //해당하는 것을 그 인덱스로 변경
+    for(var i = 0;i<babbling.length;i++){
+        canArr.forEach((element,idx)=>{
+            babbling[i]=babbling[i].replaceAll(element,idx+1);
+        })
     }
-  }
-  var onlyNumArr = [];
-  for (var i = 0; i < babbling.length; i++) {
-    //숫자만 가지고 있는 문자열만 onlyNumArr에 대입
-    if (isNaN(babbling[i]) === false) {
-      onlyNumArr.push(babbling[i]);
+    
+
+    
+    for(var i = 0;i<babbling.length;i++){
+        var check = babbling[i];
+        
+        // 모두 숫자라면
+        if(!!Number(babbling[i])){
+            
+            var check_Dul = false;
+            var before = check[0];
+            for(var checkIndex = 1;checkIndex<check.length;checkIndex++){
+                if(before === check[checkIndex]){
+                    check_Dul = true;   
+                    break;
+                }   
+                else{
+                    before = check[checkIndex];
+                }
+            }
+            
+            if(check_Dul === false){
+                answer++;
+            }
+            
+        }
     }
-  }
-  //연속된 숫자가 있는지 확인, 단 길이가 1이라면 할 필요 없이 답임.
-  for (var numCount = 0; numCount < onlyNumArr.length; numCount++) {
-    if (onlyNumArr[numCount].length === 1) {
-      answer++;
-      continue;
-    }
-    for (
-      var numIndex = 0;
-      numIndex < onlyNumArr[numCount].length - 1;
-      numIndex++
-    ) {
-      if (
-        onlyNumArr[numCount][numIndex] === onlyNumArr[numCount][numIndex + 1]
-      ) {
-        break;
-      }
-      if (numIndex === onlyNumArr[numCount].length - 2) {
-        answer++;
-      }
-    }
-  }
-  return answer;
+     
+    return answer;
 }
