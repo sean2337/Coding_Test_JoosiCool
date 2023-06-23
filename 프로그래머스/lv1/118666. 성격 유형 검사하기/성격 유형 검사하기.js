35a073da5ac56b0bf553bd95b9a898d985f6d673
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-06-23 22:26:32 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,48 +1,54 @@
 function solution(survey, choices) {
-  var answer = "";
-  var score = [];
-  //각 유형 점수 해쉬 생성
-  score["R"] = 0;
-  score["T"] = 0;
-  score["C"] = 0;
-  score["F"] = 0;
-  score["J"] = 0;
-  score["M"] = 0;
-  score["A"] = 0;
-  score["N"] = 0;
-
-  //각 요소마다 계산 시작
-  for (var i = 0; i < survey.length; i++) {
-    if (choices[i] > 4) {
-      score[survey[i][1]] += choices[i] - 4;
-    } else {
-      score[survey[i][0]] += 4 - choices[i];
+    var answer = '';
+    // 해시 테이블 만들기
+    var map = new Map();
+    map.set("R",0);map.set("C",0);map.set("J",0);map.set("A",0);
+    map.set("T",0);map.set("F",0);map.set("M",0);map.set("N",0);
+    
+    //점수계산 시작
+    for(var i = 0;i<survey.length;i++){
+        var score = choices[i];
+        let [front,back] = survey[i].split("");
+        if(score>4){
+            var value = map.get(back);
+            map.set(back,value + score - 4 );
+        }
+        else if(score<4){
+            var value = map.get(front);
+            map.set(front,value + 4- score );
+        }
     }
-  }
-  //R,T 유형도 점수 비교
-  if (score["R"] >= score["T"]) {
-    answer += "R";
-  } else {
-    answer += "T";
-  }
-  //C,F 유형도 점수 비교
-  if (score["C"] >= score["F"]) {
-    answer += "C";
-  } else {
-    answer += "F";
-  }
+    
+    if(map.get("R") >= map.get("T")){
+        answer+="R";
+    }
+    else{
+        answer+="T";
+    }
+    
+    if(map.get("C") >= map.get("F")){
+        answer+="C";
+    }
+    else{
+        answer+="F";
+    }
+    
+    
+    if(map.get("J") >= map.get("M")){
+        answer+="J";
+    }
+    else{
+        answer+="M";
+    }
+    
+    
+    if(map.get("A") >= map.get("N")){
+        answer+="A";
+    }
+    else{
+        answer+="N";
+    }
+    
 
-  //J,M 유형도 점수 비교
-  if (score["J"] >= score["M"]) {
-    answer += "J";
-  } else {
-    answer += "M";
-  }
-  //A,N 유형도 점수 비교
-  if (score["A"] >= score["N"]) {
-    answer += "A";
-  } else {
-    answer += "N";
-  }
-  return answer;
+    return answer;
 }
