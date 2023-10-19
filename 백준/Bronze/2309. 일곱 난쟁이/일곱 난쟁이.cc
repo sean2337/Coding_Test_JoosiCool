2e72d45bda993bc58444a914db40e6271f97d7e1
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-10-19 15:25:39 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -11,39 +11,38 @@ using namespace std;
 int n = 9;
 int k = 7;
 int arr[9];
+pair<int, int>findValue;
+
+int totalSum = 0;
 
 
-void combination(int start, vector<int>vec) {
-	if (vec.size() == k) {
-		int sum = 0;
-		for (int c : vec) {
-			sum += arr[c];
+void findTwo() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] + arr[j] == totalSum - 100) {
+				findValue.first = i;
+				findValue.second = j;
+				return;
+			}
 		}
-		if (sum != 100) return;
-		vector<int> sortVec;
-		for (int c : vec) {
-			cout << arr[c] << '\n';
-		}
-		exit(0);
-		return;
 	}
-
-	for (int i = start+1; i < n; i++) {
-		vec.push_back(i);
-		combination(i, vec);
-		vec.pop_back();
-	}
+	return;
 }
+
+
 
 int main() {
 
-	vector<int>vec;
 	for (int i = 0; i < 9; i++) {
 		cin >> arr[i];
+		totalSum += arr[i];
 	}
 	sort(arr, arr + 9);
-	
-	combination(-1, vec);
+	findTwo();
+	for (int i = 0; i < 9; i++) {
+		if (i == findValue.first || i == findValue.second) continue;
+		cout << arr[i] << '\n';
+	}
 
 	return 0;
 }
