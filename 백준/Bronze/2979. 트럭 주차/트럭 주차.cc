e4eac8ab657c9890c9efeca5ca603d990a5f7aa7
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-10-19 15:51:27 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,11 +1,10 @@
 #include<iostream>
 #include<string>
-#include<vector>
 using namespace std;
 
 
 int fee[4];
-bool arr[3][101];
+int arr[101];
 
 
 int main() {
@@ -20,16 +19,12 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		cin >> start >> end;
 		for (int j = start; j < end; j++) {
-			arr[i][j] = true;
+			arr[j]++;
 		}
 	}
 
-	for (int i = 0; i <= 100; i++) {
-		int count = 0;
-		if (arr[0][i]) count++;
-		if (arr[1][i]) count++;
-		if (arr[2][i]) count++;
-		result += fee[count];
+	for (int i = 0; i < 100; i++) {
+		result += fee[arr[i]];
 	}
 	cout << result << '\n';
 
