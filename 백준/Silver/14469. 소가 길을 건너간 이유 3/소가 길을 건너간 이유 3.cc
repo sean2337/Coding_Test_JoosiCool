a75f6e205d0712c9b615427e3192cb926c51acf9
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-03 14:09:29 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -1,11 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int N;
 int a, b;
 vector<pair<int, int>>vec;
-
-
 int main() {
 
 	ios_base::sync_with_stdio(false);
@@ -20,12 +17,7 @@ int main() {
 	sort(vec.begin(), vec.end());
 	int cur = vec[0].first + vec[0].second;
 	for (int i = 1; i < vec.size(); i++) {
-		if (cur <= vec[i].first) {
-			cur = vec[i].first + vec[i].second;
-		}
-		else {
-			cur += vec[i].second;
-		}
+		cur = max(vec[i].first,cur) + vec[i].second;
 	}
 	cout << cur;
 
