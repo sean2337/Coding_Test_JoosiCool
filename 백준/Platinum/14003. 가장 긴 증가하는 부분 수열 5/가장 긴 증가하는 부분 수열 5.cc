51f91d0634d050f77ef030417689dbda02872c18
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-06 23:53:28 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 656 ms, Memory: 33748 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -2,40 +2,36 @@
 using namespace std;
 typedef long long ll;
 
+ll len, N, num;
 const ll maxNum = 1000004;
-ll N, len, num;
-ll lists[maxNum];
-pair<ll, ll> vec[1000004];
+const ll INF = 1e9 + 4;
+ll lis[maxNum];
+pair<ll, ll>vec[maxNum];
 stack<ll>stk;
-const int INF = 1e9 + 4;
-
 
 int main() {
-    fill(lists, lists + 1000004, INF);
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> num;
-        auto pos = lower_bound(lists, lists + len, num);
-        ll idx = (ll)(pos - lists);
-        
-        if (*pos == INF) len++;
-        *pos = num;
-        vec[i].first = idx;
-        vec[i].second = num;
-    }
+	fill(lis, lis + maxNum, INF);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		auto pos = lower_bound(lis, lis + len, num);
+		int idx = (int)(pos - lis);
+		if (*pos == INF) len++;
+		*pos = num;
+		vec[i].first = idx;
+		vec[i].second = num;
+	}
 
-    cout << len << '\n';
+	cout << len << '\n';
+	for (int i = N - 1; i >= 0; i--) {
+		if (len - 1 == vec[i].first) {
+			len--; stk.push(vec[i].second);
+		}
+	}
+	
+	while (stk.size()) {
+		cout << stk.top() << " "; stk.pop();
+	}
 
-    for (int i = N - 1; i >= 0; i--) {
-        if (len - 1 == vec[i].first) {
-            stk.push(vec[i].second); len--;
-        }
-    }
-
-    while (stk.size())
-    {
-        cout<<stk.top()<<" "; stk.pop();
-    }
-
-    return 0;
+	return 0;
 }
