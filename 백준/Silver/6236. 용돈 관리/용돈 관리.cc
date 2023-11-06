0396abd51242382f4a5969fc1d31efbce7d7aea6
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-06 13:58:58 +0900

    [Silver II] Title: 용돈 관리, Time: 52 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll N, M;
+ll arr[300004];
+
+bool check(ll mid) {
+    ll count = 1;
+    ll sum = mid;
+    for (int i = 0; i < N; i++) {
+        if (sum - arr[i] >=0) {
+            sum -= arr[i];
+        }
+        else {
+            count++;
+            sum = mid;
+            sum -= arr[i];
+            if (sum < 0) return false;
+        }
+    }
+    return M>=count;
+}
+
+
+int main() {
+    cin >> N >> M;
+    ll l = 1, r = numeric_limits<ll>::max();
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    ll minResult = numeric_limits<ll>::max();
+    while (l <= r) {
+        ll mid = (l + r) / 2;
+        if (check(mid)) {
+            minResult = min(minResult, mid);
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+    cout << minResult;
+
+    return 0;
+}
