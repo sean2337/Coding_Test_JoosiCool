8394d9a2f22c705b05d1b45ffdf6ea9aa981363e
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-08 13:58:11 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1308 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll maxNum = 1e6 + 4;
+ll N, M, K;
+ll tree[maxNum], arr[maxNum];
+
+ll sumTree(ll idx) {
+	ll result = 0;
+	while (idx > 0) {
+		result += tree[idx];
+		idx -= (idx & -idx);
+	}
+	return result;
+}
+
+void update(ll idx, ll dif) {
+	while (idx <= N) {
+		tree[idx] += dif;
+		idx += (idx & -idx);
+	}
+}
+
+int main() {
+
+	cin >> N >> M >> K;
+	for (ll i = 1; i <= N; i++) {
+		cin >> arr[i];
+		update(i, arr[i]);
+	}
+
+	ll a, b, c;
+	for (ll i = 1; i <= M + K; i++) {
+		cin >> a >> b >> c;
+		if (a == 1) {
+			ll dif = c - arr[b];
+			arr[b] = c;
+			update(b, dif);
+		}
+		else {
+			cout << sumTree(c) - sumTree(b - 1) << '\n';
+		}
+	}
+	return 0;
+}
