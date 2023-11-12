4467392a9d54420651afb3855193c86f07831d8f
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-12 14:45:23 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,38 +1,40 @@
+// 가중치가 다른데, 한 곳에서만 출발하는 경우
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<long long, long long>pairLong;
+typedef pair<ll, ll> pairll;
 const ll INF = 987654321;
-ll V, E, K, n, k, m;
 ll dist[104][104];
-
+ll N, M, a, b, c;
 
 int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	fill(&dist[0][0], &dist[0][0] + 104 * 104, INF);
 
-	cin >> V >> E;
-	for (ll i = 0; i < E; i++) {
-		cin >> n >> k >> m;
-		n--; k--;
-		dist[n][k] = min(dist[n][k], m);
+	cin >> N >> M;
+	for (ll i = 0; i < M; i++) {
+		cin >> a >> b >> c;
+		a--; b--;
+		dist[a][b] = min(dist[a][b],c);
 	}
 
-	for (ll k = 0; k < V; k++) {
-		for (ll i = 0; i < V; i++) {
-			for (ll j = 0; j < V; j++) {
+	for (ll k = 0; k < N; k++) {
+		for (ll i = 0; i < N; i++) {
+			for (ll j = 0; j < N; j++) {
 				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 			}
 		}
 	}
 
-	for (ll i = 0; i < V; i++) {
-		for (ll j = 0; j < V; j++) {
-			if (i == j || dist[i][j] == INF) cout << "0 ";
+	for (ll i = 0; i < N; i++) {
+		for (ll j = 0; j < N; j++) {
+			if (i == j || dist[i][j] == INF) cout << 0 << " ";
 			else cout << dist[i][j] << " ";
 		}
 		cout << '\n';
 	}
-
 	return 0;
 }
