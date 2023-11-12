562c460cee9c4b6a5991c1acefde05bc142a4cae
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-12 14:33:57 +0900

    [Gold IV] Title: 최단경로, Time: 116 ms, Memory: 15404 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,43 +1,55 @@
+// 가중치가 다른데, 한 곳에서만 출발하는 경우
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> pairll;
-ll V, E, K, u, v, w;
-vector<pair<ll, ll>>adj[20001];
-ll dist[20001];
 const ll INF = 987654321;
-priority_queue<pairll, vector<pairll>, greater<pairll>> pq;
+ll dist[20004];
+vector<pairll>adj[20004];
+priority_queue<pairll, vector<pairll>, greater<pairll>> que;
+ll N, M, a, b, c, K;
+
 
 int main() {
-	cin >> V >> E >> K;
-	fill(dist, dist + 20001, INF);
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	for (int i = 0; i < E; i++) {
-		cin >> u >> v >> w;
-		adj[u].push_back({ w,v });
+	cin >> N >> M >> K;
+
+	for (ll i = 0; i < M; i++) {
+		cin >> a >> b >> c;
+		adj[a].push_back({ b,c });
 	}
 
-	pq.push({ 0,K });
+	fill(dist, dist + 20004, INF);
+
 	dist[K] = 0;
-	while (pq.size()) {
-		ll here = pq.top().second;
-		ll here_dist = pq.top().first;
-		pq.pop();
+	que.push({ 0,K });
+	while (que.size()) {
+		ll here_dist = que.top().first;
+		ll here = que.top().second;
+		que.pop();
 		if (dist[here] != here_dist) continue;
 		for (pairll there : adj[here]) {
-			ll _dist = there.first;
-			ll _there = there.second;
-			if (dist[_there] > dist[here] + _dist) {
-				dist[_there] = dist[here] + _dist;
-				pq.push({ dist[_there], _there });
+			ll there_dist = there.second;
+			ll _there = there.first;
+
+			if (dist[here] + there_dist < dist[_there]) {
+				dist[_there] = dist[here] + there_dist;
+				que.push({ dist[_there], _there });
 			}
 		}
 	}
-
-	for (int i = 1; i <= V; i++) {
-		if (dist[i] == INF) cout << "INF" << '\n';
-		else cout << dist[i] << '\n';
+	
+	for (ll i = 1; i <= N; i++) {
+		if (dist[i] == INF) {
+			cout << "INF";
+		}
+		else cout << dist[i];
+		cout << '\n';
 	}
+
+
 	return 0;
 }
-
