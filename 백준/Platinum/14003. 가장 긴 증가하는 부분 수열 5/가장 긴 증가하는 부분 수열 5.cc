74467a503b1b3d8beb463261ca337dda701045d8
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-15 13:48:55 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 656 ms, Memory: 25940 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,15 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
-ll len, N, num;
 const ll maxNum = 1000004;
-const ll INF = 1e9 + 4;
-ll lis[maxNum];
-pair<ll, ll>vec[maxNum];
+
+const ll INF = -1e18;
+ll len, N, num;
+ll lis[ maxNum+ 3];
+vector<pair<int,int>>vec(maxNum + 3);
 stack<ll>stk;
 
 int main() {
+
 	fill(lis, lis + maxNum, INF);
 	cin >> N;
 	for (int i = 0; i < N; i++) {
@@ -18,17 +19,16 @@ int main() {
 		int idx = (int)(pos - lis);
 		if (*pos == INF) len++;
 		*pos = num;
-		vec[i].first = idx;
-		vec[i].second = num;
+		vec[i].first = idx; vec[i].second = num;
 	}
-
 	cout << len << '\n';
 	for (int i = N - 1; i >= 0; i--) {
-		if (len - 1 == vec[i].first) {
-			len--; stk.push(vec[i].second);
+		if (len - 1== vec[i].first) {
+			stk.push(vec[i].second);
+			len--;
 		}
 	}
-	
+
 	while (stk.size()) {
 		cout << stk.top() << " "; stk.pop();
 	}
