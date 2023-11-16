def7a58d46a20287d7c0766ba6396f79aa54cfe9
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-17 02:04:56 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pairll;
+map<char, ll>mp;
+ll N, M;
+ll arr[104];
+ll a, b, c;
+
+int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N >> M;
+	for (ll i = 1; i <= N; i++) {
+		arr[i] = i;
+	}
+	for (ll i = 0; i < M; i++) {
+		cin >> a >> b;
+		swap(arr[a], arr[b]);
+	}
+	for (ll i = 1; i <= N; i++) {
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
