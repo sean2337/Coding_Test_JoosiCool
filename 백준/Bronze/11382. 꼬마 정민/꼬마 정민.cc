4fe95015e212e8f408a69d63e357c130254e8c2c
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-17 01:47:59 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ll a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+
+	return 0;
+}
