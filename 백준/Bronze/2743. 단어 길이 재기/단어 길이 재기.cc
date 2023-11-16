3b0f9ab7aa1e5c4d7a8d7c8bd47579d4c56d2730
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-17 02:09:46 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pairll;
+map<char, ll>mp;
+ll N, M;
+ll arr[104];
+ll a, b, c;
+
+int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	string s;
+	cin >> s;
+	cout << s.size();
+
+	return 0;
+}
