cb6803443c0cfe990e5e7f2968a6d0ad22009f56
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-18 15:03:50 +0900

    [Gold I] Title: 구간 합 구하기, Time: 232 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,46 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include<bits/stdc++.h>
+#define endl "\n"
 using namespace std;
 typedef long long ll;
-typedef pair<long long, long long>pairLong;
-
-ll N, M, K, a, b, c;
-ll arr[1000004];
-ll sumArr[1000004];
+typedef pair<ll, ll> pairll;
+ll visited[10004];
+const ll INF = 987654321;
+ll N, K, M, num;
+ll a, b, c;
+const ll maxNum = 1e6 + 4;
+ll arr[maxNum];
+ll sum[maxNum];
 
 void update(ll idx, ll dif) {
 	while (idx <= N) {
-		sumArr[idx] += dif;
-		idx += idx & (-idx);
+		sum[idx] += dif;
+		idx += (idx & -idx);
 	}
 }
 
-ll sum(ll idx) {
-	ll result = 0;
+ll getSum(ll idx) {
+	ll rnt = 0;
 	while (idx > 0) {
-		result += sumArr[idx];
-		idx -= idx & (-idx);
+		rnt += sum[idx];
+		idx -= (idx & -idx);
 	}
-	return result;
+	return rnt;
 }
-
 
 int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	cin >> N >> M >> K;
+	cin >> N >> K >> M;
 	for (ll i = 1; i <= N; i++) {
 		cin >> arr[i];
 		update(i, arr[i]);
 	}
-	for (int i = 1; i <= M + K; i++) {
+
+	for (ll i = 1; i <= K+M; i++) {
 		cin >> a >> b >> c;
 		if (a == 1) {
-			update(b, c - arr[b]);
+			ll dif = c - arr[b];
+			update(b, dif);
 			arr[b] = c;
 		}
 		else {
-			cout << sum(c) - sum(b - 1) << '\n';
+			cout << getSum(c) - getSum(b - 1) << '\n';
 		}
 	}
+
 
 
 	return 0;
