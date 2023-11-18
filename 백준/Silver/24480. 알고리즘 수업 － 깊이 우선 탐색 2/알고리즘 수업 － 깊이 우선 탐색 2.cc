4dd2bcc219ff3535aaa6c9c5015905dab4f32fdf
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-18 16:16:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 236 ms, Memory: 11332 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pairll;
+const ll INF = 987654321;
+ll N, M, R, num, a, b, c;
+vector<ll> adj[100004];
+ll arr[100004];
+bool visited[100004];
+ll cnt = 0;
+
+bool compare(ll a, ll b) {
+	return a > b;
+}
+
+void DFS(ll idx) {
+	cnt++;
+	arr[idx] = cnt;
+	for (ll next : adj[idx]) {
+		if (visited[next])continue;
+		visited[next] = 1;
+		DFS(next);
+	}
+}
+
+
+int main() {
+
+	cin >> N >> M >> R;
+	for (ll i = 0; i < M; i++) {
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+
+	for (ll i = 1; i <= N; i++) {
+		sort(adj[i].begin(), adj[i].end(),compare);
+	}
+
+	visited[R] = 1;
+	DFS(R);
+
+	for (ll i = 1; i <= N; i++) {
+		cout << arr[i] << '\n';
+	}
+
+	return 0;
+}
