52f737641eb58be752af3ed9f657b25caf75f91e
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-18 23:39:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -2,23 +2,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> pairll;
-const ll max_Size = 104;
+typedef pair<ll, ll> pairll;
+const ll INF = 987654321;
+ll N, K, M, num, a, b, c;
+const ll maxSize = 30;
 
-ll T, N, M, K, a, b;
-bool visited[max_Size][max_Size];
-ll arr[max_Size][max_Size];
+ll visited[maxSize][maxSize];
+ll arr[maxSize][maxSize];
 ll dx[] = { 1,0,-1,0 };
-ll dy[] = { 0,1,0,-1 };
-ll cnt;
-vector<ll>vec;
+ll dy[] = { 0,-1,0,1 };
+vector<ll> vec;
+ll cnt = 0;
+
 
 void DFS(ll y, ll x) {
-	for (ll i = 0; i < 4; i++) {
-		ll ny = y + dy[i];
+	for (ll i = 0; i < 4; i++)
+	{
 		ll nx = x + dx[i];
-		if (ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx]) continue;
-		if (arr[ny][nx] == 0) continue;
+		ll ny = y + dy[i];
+		if (ny<0 || ny>=N || nx < 0 || nx >= N||visited[ny][nx]) continue;
+		if (arr[ny][nx] != 1) continue;
 		visited[ny][nx] = 1;
 		cnt++;
 		DFS(ny, nx);
@@ -26,35 +29,30 @@ void DFS(ll y, ll x) {
 }
 
 
-
-
 int main() {
-	
-		ll rnt = 0;
-		cin >> N ;
-		for (ll i = 0; i < N; i++) {
-			for (ll j = 0; j < N; j++) {
-				scanf("%1d", &arr[i][j]);
+
+	cin >> N;
+	for (ll i = 0; i < N; i++) {
+		for (ll j = 0; j < N; j++) {
+			scanf("%1d", &arr[i][j]);
+		}
+	}
+
+	for (ll i = 0; i < N; i++) {
+		for (ll j = 0; j < N; j++) {
+			if (arr[i][j] == 1 && !visited[i][j]) {
+				cnt = 1;
+				visited[i][j] = true;
+				DFS(i, j);
+				vec.push_back(cnt);
 			}
 		}
-
-		for (ll i = 0; i < N; i++) {
-			for (ll j = 0; j < N; j++) {
-				if (arr[i][j] == 1 && !visited[i][j]) {
-					rnt++;
-					cnt = 1;
-					visited[i][j] = 1;
-					DFS(i, j);
-					vec.push_back(cnt);
-				}
-			}
-		}
-		sort(vec.begin(), vec.end());
-		cout << rnt << '\n';
-		for (ll i : vec) {
-			cout << i << "\n";
-		}
-
+	}
+	cout << vec.size() << '\n';
+	sort(vec.begin(), vec.end());
+	for (ll i : vec) {
+		cout << i << " ";
+	}
 
 
 	return 0;
