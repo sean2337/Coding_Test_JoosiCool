4737d823bf2f082446b9dfa352d19a54380af5f0
Juicycool <100525337+sean2337@users.noreply.github.com>
2023-11-19 19:36:25 +0900

    [Gold IV] Title: 최단경로, Time: 108 ms, Memory: 15404 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,55 +1,57 @@
-// 가중치가 다른데, 한 곳에서만 출발하는 경우
+#define _CRT_SECURE_NO_WARNINGS 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> pairll;
-const ll INF = 987654321;
-ll dist[20004];
-vector<pairll>adj[20004];
-priority_queue<pairll, vector<pairll>, greater<pairll>> que;
-ll N, M, a, b, c, K;
+const ll maxSize = 1e8 + 4;
+const ll INF = 1e18;
+ll N, K, M, T, V, E, num, a, b, c;
+ll dis[20004];
+vector<pairll> adj[20004];
+priority_queue<pairll, vector<pairll>, greater<pairll>>que;
 
+
+
+
+// 다익스트라
 
 int main() {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N >> M >> K;
-
-	for (ll i = 0; i < M; i++) {
+	cin >> V >> E >> K;
+	for (ll i = 0; i < E; i++) {
 		cin >> a >> b >> c;
-		adj[a].push_back({ b,c });
+		adj[a].push_back({ c,b });
 	}
 
-	fill(dist, dist + 20004, INF);
-
-	dist[K] = 0;
+	fill(dis, dis + 20004, INF);
+	dis[K] = 0;
 	que.push({ 0,K });
 	while (que.size()) {
-		ll here_dist = que.top().first;
 		ll here = que.top().second;
+		ll here_dist = que.top().first;
 		que.pop();
-		if (dist[here] != here_dist) continue;
-		for (pairll there : adj[here]) {
-			ll there_dist = there.second;
-			ll _there = there.first;
-
-			if (dist[here] + there_dist < dist[_there]) {
-				dist[_there] = dist[here] + there_dist;
-				que.push({ dist[_there], _there });
+		if (dis[here] != here_dist) continue;
+		for (pairll _there : adj[here]) {
+			ll there = _there.second;
+			ll there_dist = _there.first;
+			if (dis[there] > dis[here] + there_dist) {
+				dis[there] = dis[here] + there_dist;
+				que.push({ dis[there], there });
 			}
 		}
 	}
-	
-	for (ll i = 1; i <= N; i++) {
-		if (dist[i] == INF) {
-			cout << "INF";
-		}
-		else cout << dist[i];
-		cout << '\n';
-	}
 
+	for (ll i = 1; i <= V; i++) {
+		if (dis[i] == INF) {
+			cout << "INF" << '\n';
+		}
+		else {
+			cout << dis[i] << '\n';
+		}
+	}
 
 	return 0;
 }
