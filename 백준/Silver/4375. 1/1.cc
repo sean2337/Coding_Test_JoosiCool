c04bad5b64a062affae2a8a00d5918a32777e823
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-05-04 16:45:07 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,32 +1,24 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<array>
-#include <limits>
-#include<map>
-#include<set>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+ll N, M;
+string s;
 
-
-
-int main() {
-
-	int n;
-
-	while (cin>>n) {
-
-		int check = 1;
-		int count = 1;
-		//0일때까지 계속함.
-		while (!(check % n == 0)) {
-			check = check * 10 + 1;
-			check %= n;
-			count++;
-		}
-		cout << count << '\n';
-	}
-
-
-	return 0;
+int main()
+{
+    while (cin >> N)
+    {
+        ll num = 1;
+        ll count = 1;
+        while (true)
+        {
+            if (num % N == 0)
+                break;
+            num %= N;
+            count++;
+            num = num * 10 + 1;
+        }
+        cout << count << '\n';
+    }
+    return 0;
 }
