537c5f98cc22d9985079638cbca89f33b7952dd6
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-05-04 16:35:43 +0900

    [Silver IV] Title: 주몽, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 typedef long long ll;
-
-ll N,M, num;
-ll arr[10000004];
-ll l, r, cnt;
-
-int main() {
-
-    vector<ll>vec;
-    cin>>N>>M;
-
-    for(ll i = 0;i<N;i++){
-        cin>>arr[i];
+ll N, M;
+string s;
+ll arr[20000];
+ll rnt;
+int main()
+{
+    cin >> N >> M;
+    for (ll i = 0; i < N; i++)
+    {
+        cin >> arr[i];
     }
-
     sort(arr, arr + N);
-
-    l = 0; r = N-1;
-    while(l<r){
-        if(arr[l]+arr[r]==M){
-            l++; r--; cnt++;
-        }
-        else if(arr[l]+arr[r]>M){
+    ll l = 0, r = N - 1;
+    while (l < r)
+    {
+        if (arr[l] + arr[r] == M)
+        {
+            rnt++;
+            l++;
             r--;
         }
-        else{
+        else if (arr[l] + arr[r] < M)
+        {
             l++;
         }
+        else
+        {
+            r--;
+        }
     }
+    cout << rnt;
 
-    if(l==r && arr[l] == 9) cnt++;
-    cout<<cnt;
-
-    return 0; 
+    return 0;
 }
