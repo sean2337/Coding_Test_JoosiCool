c039c3e0ba600380de5aa5435f5066845bd7d442
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-05-07 23:18:33 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -1,49 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+ll N, M, T, K;
+string s;
+char arr[104][104];
 
+ll solveDis(ll y, ll x)
+{
+    ll dis = 0;
+    while (true)
+    {
+        if (arr[y][x] == 'c')
+        {
+            return dis;
+        }
+        x--;
+        dis++;
+        if (x < 0)
+            return -1;
+    }
+}
 
-int yLen, xLen;
-int x, y;
-int visited[103][103];
+int main()
+{
+    cin >> N >> M;
+    for (ll i = 0; i < N; i++)
+    {
+        cin >> s;
+        for (ll j = 0; j < M; j++)
+        {
+            arr[i][j] = s[j];
+        }
+    }
 
+    for (ll i = 0; i < N; i++)
+    {
+        for (ll j = 0; j < M; j++)
+        {
+            cout << solveDis(i, j) << " ";
+        }
+        cout << '\n';
+    }
 
-int main() {
-	cin >> yLen >> xLen;
-	char c;
-	queue<pair<int, int>>que;
-	memset(visited, -1, sizeof(visited));
-	for (int i = 0; i < yLen; i++) {
-		for (int j = 0; j < xLen; j++) {
-			cin >> c;
-			if (c == 'c') {
-				que.push({ i,j });
-				visited[i][j] = 0;
-			}
-		}
-	}
-
-	while (!que.empty()) {
-		int queSize = que.size();
-		for (int i = 0; i < queSize; i++) {
-			tie(y, x) = que.front();
-			que.pop();
-
-			int nx = x + 1;
-			int ny = y;
-
-			if (nx >= xLen || visited[ny][nx] !=-1) continue;
-			visited[ny][nx] = visited[y][x] + 1;
-			que.push({ ny,nx });
-		}
-	}
-
-	
-	for (int i = 0; i < yLen; i++) {
-		for (int j = 0; j < xLen; j++) {
-			cout << visited[i][j] << " ";
-		}
-		cout << '\n';
-	}
-	
-	return 0;
-};
+    return 0;
+}
