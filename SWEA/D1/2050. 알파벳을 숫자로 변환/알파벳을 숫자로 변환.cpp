53d4c2ab79bfeb007e3f0227b7b364a5baece3b9
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-05-10 15:27:17 +0900

    [D1] Title: 알파벳을 숫자로 변환, Time: 7 ms, Memory: 13,376 KB -BaekjoonHub

---
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pairll;
+const ll maxSize = 1e8 + 4;
+const ll INF = 987654321;
+ll N, K, M, T, V, E, num, a, b, c, rnt;
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> s;
+	for (char c : s) {
+		cout << c - 64 << " ";
+	}
+
+	return 0;
+};
