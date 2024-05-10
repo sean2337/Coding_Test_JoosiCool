5d8a520eb652791775cf7eedc5369bf555629865
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-05-10 15:12:34 +0900

    [D1] Title: 대각선 출력하기, Time: 6 ms, Memory: 13,376 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pairll;
+const ll maxSize = 1e8 + 4;
+const ll INF = 987654321;
+ll N, K, M, T, V, E, num, a, b, c, rnt;
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "#++++\n+#+++\n++#++\n+++#+\n++++#";
+
+	return 0;
+};
