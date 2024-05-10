893d66475c89f30b68f24becf903c2bf8d839b85
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-05-10 15:05:49 +0900

    [D1] Title: 서랍의 비밀번호, Time: 6 ms, Memory: 13,336 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pairll;
+const ll maxSize = 1e8 + 4;
+const ll INF = 987654321;
+ll N, K, M, T, V, E, num, a, b, c;
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> a >> b;
+	cout << abs(a - b) + 1;
+
+	return 0;
+};
