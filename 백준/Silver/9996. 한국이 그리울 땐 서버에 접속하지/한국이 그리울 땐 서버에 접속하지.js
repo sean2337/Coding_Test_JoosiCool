c877fb164b008260afe992b6102f95d788cf74f3
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-06-27 14:14:40 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 112 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.js
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.js
@@ -1,18 +1,22 @@
-const fs = require(`fs`);
-const inputData = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'testcase.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
 
-var count = Number(inputData.shift());
+let inCount = Number(input[0]);
+let [f, e] = input[1].split('*');
+let result = '';
 
-var [startPattern, endPattern] = inputData.shift().split("*");
+function checkDA(str) {
+  if (str.length < f.length + e.length) return 'NE';
+  let frontIndex = str.indexOf(f);
+  let endIndex = str.lastIndexOf(e);
+  if (frontIndex === 0 && endIndex === str.length - e.length) return 'DA';
+  else return 'NE';
+}
 
-const regex = new RegExp(`^${startPattern}.*${endPattern}$`, 'gi');
-
-inputData.forEach((element)=>{
-
-    if(element.search(regex) !== -1){
-        console.log("DA");
-    }
-    else{
-        console.log("NE");
-    }
-})
+for (let i = 2; i <= inCount + 1; i++) {
+  let checkStr = input[i];
+  result += checkDA(checkStr);
+  result += '\n';
+}
+return console.log(result);
