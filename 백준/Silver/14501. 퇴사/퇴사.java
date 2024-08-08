0c79a95bb4a381a23cabd02d087da5fca7ae2010
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-08-09 00:00:57 +0900

    [Silver III] Title: 퇴사, Time: 212 ms, Memory: 18392 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {	
+	static int maxSize = 20004;
+	static int[][] arr = new int[maxSize][2];
+	static int[] dp = new int[maxSize];
+	static int N, T, P, maxValue;
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	
+		N = scanner.nextInt();
+		for(int i = 0; i<N;i++) {
+			arr[i][0] = scanner.nextInt();
+			arr[i][1] = scanner.nextInt();
+		}
+		
+		
+		for(int i = 0; i<=N;i++) {
+			dp[i] = Math.max(maxValue, dp[i]);
+			
+			T = arr[i][0]; P = arr[i][1];
+			dp[i+T] = Math.max(dp[i+T], dp[i] + P);
+			
+			maxValue = Math.max(maxValue, dp[i]);
+		}
+		
+		System.out.println(maxValue);
+		scanner.close();	
+	}
+}
