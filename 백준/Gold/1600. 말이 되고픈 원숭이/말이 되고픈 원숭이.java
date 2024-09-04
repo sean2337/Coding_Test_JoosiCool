0a753c7e77747607199eb2503536d77354778e95
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-09-04 09:42:26 +0900

    Update 말이 되고픈 원숭이.java

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -5,12 +5,11 @@
 import java.util.*;
 
 class Point {
-    int x, y, dist, k;
+    int x, y, k;
 
-    Point(int x, int y, int dist, int k) {
+    Point(int x, int y, int k) {
         this.x = x;
         this.y = y;
-        this.dist = dist;
         this.k = k;
     }
 }
@@ -20,7 +19,7 @@ public class Main {
     static final int MAX = 200;
     static int K, W, H;
     static int[][] arr = new int[MAX][MAX];
-    static boolean[][][] visited = new boolean[MAX][MAX][31];
+    static int[][][] visited = new int[MAX][MAX][31]; 
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static int[] horseDX = {-2, -1, 1, 2, 2, 1, -1, -2};
@@ -28,40 +27,41 @@ public class Main {
 
     public static int bfs() {
         Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(0, 0, 0, 0));
-        visited[0][0][0] = true;
+        q.offer(new Point(0, 0, 0));
+        visited[0][0][0] = 0; 
 
         while (!q.isEmpty()) {
             Point cPoint = q.poll();
+            int currentDist = visited[cPoint.y][cPoint.x][cPoint.k];
 
             if (cPoint.x == W - 1 && cPoint.y == H - 1) {
-                return cPoint.dist;
+                return currentDist;
             }
 
             for (int i = 0; i < 4; i++) {
                 int nx = cPoint.x + dx[i];
                 int ny = cPoint.y + dy[i];
 
-                if (nx >= 0 && ny >= 0 && nx < W && ny < H && arr[ny][nx] == 0 && !visited[ny][nx][cPoint.k]) {
-                    visited[ny][nx][cPoint.k] = true;
-                    q.offer(new Point(nx, ny, cPoint.dist + 1, cPoint.k));
+                if (nx >= 0 && ny >= 0 && nx < W && ny < H && arr[ny][nx] == 0 && visited[ny][nx][cPoint.k] > currentDist + 1) {
+                    visited[ny][nx][cPoint.k] = currentDist + 1;
+                    q.offer(new Point(nx, ny, cPoint.k));
                 }
             }
-
+            
             if (cPoint.k < K) {
                 for (int i = 0; i < 8; i++) {
                     int nx = cPoint.x + horseDX[i];
                     int ny = cPoint.y + horseDY[i];
 
-                    if (nx >= 0 && ny >= 0 && nx < W && ny < H && arr[ny][nx] == 0 && !visited[ny][nx][cPoint.k + 1]) {
-                        visited[ny][nx][cPoint.k + 1] = true;
-                        q.offer(new Point(nx, ny, cPoint.dist + 1, cPoint.k + 1));
+                    if (nx >= 0 && ny >= 0 && nx < W && ny < H && arr[ny][nx] == 0 && visited[ny][nx][cPoint.k + 1] > currentDist + 1) {
+                        visited[ny][nx][cPoint.k + 1] = currentDist + 1;
+                        q.offer(new Point(nx, ny, cPoint.k + 1));
                     }
                 }
             }
         }
 
-        return -1;
+        return -1;  
     }
 
     public static void main(String[] args) {
@@ -74,6 +74,12 @@ public class Main {
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
                 arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                Arrays.fill(visited[i][j], INF);
             }
         }
 
