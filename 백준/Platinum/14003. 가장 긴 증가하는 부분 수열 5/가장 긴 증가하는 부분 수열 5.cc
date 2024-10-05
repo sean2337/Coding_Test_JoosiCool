13dd421a14b0a14d10edae1c14ac34369888e9c1
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-10-05 22:04:10 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 204 ms, Memory: 33860 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,45 +1,47 @@
-#define _CRT_SECURE_NO_WARNINGS 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> pairll;
-const ll maxSize = 1e6 + 4;
-const ll INF = 1e18;
-ll N,len, K, M, T, num, a, b, c;
-ll lis[maxSize];
-pairll vec[maxSize];
-stack<ll>stk;
 
+const ll maxN = 1e6 + 4;
+const ll INF = 1e18;
+ll N, len, num;
+ll lis[maxN];
+pairll arr[maxN];
+stack<ll> stk;
 
 
 int main() {
+	
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	fill(lis, lis + maxSize, INF);
 	cin >> N;
+	fill(lis, lis + maxN, INF);
+
 	for (ll i = 0; i < N; i++) {
 		cin >> num;
-		auto pos = lower_bound(lis, lis + len, num);
-		if (*pos == INF) len++;
-		*pos = num;
-		ll idx = ll(pos - lis);
-		vec[i].first = idx;
-		vec[i].second = num;
+		auto iter = lower_bound(lis, lis + len, num);
+		if (*iter == INF) len++;
+		*iter = num;
+		ll idx = iter - lis;
+
+		arr[i].first = idx;
+		arr[i].second = num;
 	}
+	cout << len << "\n";
 
-	cout << len << '\n';
-
-	for (ll i = N - 1; i >= 0; i--) {
-		if (len - 1 == vec[i].first) {
-			stk.push(vec[i].second);
+	for (int i = N - 1; i >= 0; i--) {
+		if (len - 1 == arr[i].first) {
 			len--;
+			stk.push(arr[i].second);
 		}
 	}
 
 	while (stk.size()) {
-		cout << stk.top() << " "; stk.pop();
+		cout << stk.top() << " ";
+		stk.pop();
 	}
 
 	return 0;
