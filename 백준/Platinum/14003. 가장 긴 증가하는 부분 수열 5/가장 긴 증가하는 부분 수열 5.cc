42b032a8e393c368378fa3b316b6aee8c048d8ef
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-10-06 08:32:27 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 200 ms, Memory: 33860 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -5,37 +5,38 @@ typedef pair<ll, ll> pairll;
 
 const ll maxN = 1e6 + 4;
 const ll INF = 1e18;
-ll N, len, num;
-ll lis[maxN];
-pairll arr[maxN];
-stack<ll> stk;
 
+ll lis[maxN];
+ll N, len, num;
+pairll arr[maxN];
 
 int main() {
 	
-	ios_base::sync_with_stdio(NULL);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N;
 	fill(lis, lis + maxN, INF);
 
+	cin >> N;
 	for (ll i = 0; i < N; i++) {
 		cin >> num;
-		auto iter = lower_bound(lis, lis + len, num);
-		if (*iter == INF) len++;
-		*iter = num;
-		ll idx = iter - lis;
-
+		auto pos = lower_bound(lis, lis + len, num);
+		if (*pos == INF) len++;
+		*pos = num;
+		ll idx = pos - lis;
 		arr[i].first = idx;
 		arr[i].second = num;
 	}
+
 	cout << len << "\n";
 
-	for (int i = N - 1; i >= 0; i--) {
-		if (len - 1 == arr[i].first) {
-			len--;
+	stack<ll> stk;
+
+	for (ll i = N - 1; i >= 0; i--) {
+		if (arr[i].first == len - 1) {
 			stk.push(arr[i].second);
+			len--;
 		}
 	}
 
