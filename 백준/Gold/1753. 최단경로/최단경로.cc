2157c72a63364e98e6f3e7509b97a57e1361c4f3
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-10-06 08:25:13 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 15408 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,58 +1,55 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
 typedef pair<ll, ll> pairll;
-const ll maxSize = 1e18 + 4;
+
+
+const ll maxV = 20004;
 const ll INF = 1e18;
-ll N, K, M, T, V, E, num, a, b, c;
-const ll maxN = 20004;
-ll dis[maxN];
-vector<pairll> adj[20004];
-priority_queue < pairll, vector<pairll>, greater<pairll>> que;
+ll dis[maxV];
+vector<pairll> adj[maxV];
+priority_queue<pairll, vector<pairll>, greater<pairll>> que;
+
+ll V, E, K, a, b, c;
 
 
 int main() {
+	
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-  
-    cin >> V >> E >> K;
-    for (ll i = 0; i < E; i++) {
-        cin >> a >> b >> c;
-        adj[a].push_back({ c,b });
-    }
+	cin >> V >> E >> K;
+	for (ll i = 0; i < E; i++) {
+		cin >> a >> b >> c;
+		adj[a].push_back({ c,b });
+	}
 
-    fill(dis, dis + maxN, INF);
-    dis[K] = 0;
-    que.push({ 0, K });
+	fill(dis, dis + maxV, INF);
+	dis[K] = 0;
+	que.push({ 0,K });
 
-    while (que.size()) {
-        ll here = que.top().second;
-        ll here_dist = que.top().first;
-        que.pop();
+	while (que.size()) {
+		ll here = que.top().second;
+		ll here_dist = que.top().first;
+		que.pop();
 
-        if (dis[here] != here_dist) continue;
-        for (pairll _there : adj[here]) {
-            ll there = _there.second;
-            ll there_dist = _there.first;
+		if (dis[here] != here_dist) continue;
 
-            if (dis[there] > dis[here] + there_dist) {
-                dis[there] = dis[here] + there_dist;
-                que.push({ dis[there], there });
-            }
-        }
-    }
-   
-    for (ll i = 1; i <= V; i++) {
-        if (dis[i] == INF) {
-            cout << "INF" << '\n';
-        }
-        else {
-            cout << dis[i] << "\n";
-        }
-    }
+		for (pairll _there : adj[here]) {
+			ll there = _there.second;
+			ll there_dist = _there.first;
 
-    return 0;
+			if (dis[there] > dis[here] + there_dist) {
+				dis[there] = dis[here] + there_dist;
+				que.push({ dis[there], there });
+			}
+		}
+	}
+
+	for (ll i = 1; i <= V; i++) {
+		cout << (dis[i] == INF ? "INF" : to_string(dis[i])) << '\n';
+	}
+
+	return 0;
 }
