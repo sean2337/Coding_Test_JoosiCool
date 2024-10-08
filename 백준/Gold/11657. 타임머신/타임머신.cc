36a048a9e85ad6d3c149e7d66401b772335c8d5a
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-10-08 10:47:14 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,51 +1,59 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include<bits/stdc++.h>
-#define endl "\n"
+#include<iostream>
+#include<queue>
+#include <utility>
 using namespace std;
+
 typedef long long ll;
 typedef pair<ll, ll> pairll;
-const ll INF = 987654321;
-ll N, K, M, num, a, b, c;
-ll dis[504];
-vector<pairll> adj[504];
+const ll maxN = 1e8 + 4;
+const ll maxM = 504;
+const ll INF = 1e18;
+
+vector<pairll> adj[maxM];
+ll dis[maxM];
+
+
+ll N, M, a, b, c;
 
 
 int main() {
-	ios_base::sync_with_stdio(NULL);
+
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	fill(&dis[0], &dis[0] + 504, INF);
 	cin >> N >> M;
 	for (ll i = 0; i < M; i++) {
 		cin >> a >> b >> c;
-		a--; b--;
 		adj[a].push_back({ c,b });
 	}
 
-	dis[0] = 0;
-	queue<ll>que;
+	fill(dis, dis + maxM, INF);
+	dis[1] = 0;
 
+	queue<ll>que;
 	for (ll i = 0; i < N; i++) {
-		for (ll here = 0; here < N; here++) {
-			for (pairll _there : adj[here]) {
-				ll there = _there.second; ll there_dist = _there.first;
-				if ( dis[here] != INF && dis[there] > dis[here] + there_dist) {
+		for (ll here = 1; here <= N; here++) {
+			for (pairll _there:adj[here]) {
+				ll there = _there.second;
+				ll there_dist = _there.first;
+				if (dis[here] != INF && dis[there] > dis[here] + there_dist) {
 					dis[there] = dis[here] + there_dist;
-					if (i == N - 1)que.push(here);
+					if (i == N - 1) que.push(here);
 				}
 			}
 		}
 	}
 	if (que.size()) {
-		cout << -1; return 0;
+		cout << -1;
+		return 0;
 	}
-	for (ll i = 1; i < N; i++) {
-		if (dis[i] == INF) {
-			cout << -1 << '\n';
-		}
-		else cout << dis[i] << '\n';
+
+	for (ll i = 2; i <= N; i++) {
+		if (dis[i] == INF) cout << -1 << "\n";
+		else cout << dis[i] << "\n";
 	}
+
 
 	return 0;
 }
